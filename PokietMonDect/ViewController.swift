3ef27637f75aa12d50223968bdb6c710ef962f26
Hsin <hooy123456@gmail.com>
2016-07-27 07:40:10 +0800

    part7 Audio success

--- PokietMonDect/ViewController.swift
+++ PokietMonDect/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,
     UICollectionViewDelegateFlowLayout
@@ -14,7 +16,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    
+    var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,19 +26,52 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collectionView.delegate = self
         collectionView.dataSource = self
         
+    
+        self.parsePokeMonCSV()
+        self.initAudio()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
     }
 
+    //MARK: CSV parsing
+    func parsePokeMonCSV() {
+        //first: grabing the path of pokemon.csv
+        let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
+        //second: to throw error
+        do{
+            //if sucess
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            print(rows)
+            
+            for row in rows{
+                let pokeID = Int(row["id"]!)!
+                let name = row["identifier"]!
+                let poke = Pokemon(name: name, pokedexID: pokeID)
+                pokemon.append(poke)
+            }
+            print(self.pokemon)
+
+            
+        }catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+    }
+    
+    
+    //MARK: CollectionView
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
             
-            let pokemon = Pokemon(name: "test", pokedexID: indexPath.row + 1)
-            cell.configureCell(pokemon)
+            var poke = pokemon[indexPath.row]
+            
+            cell.configureCell(poke)
             
             return cell
         }else{
@@ -58,15 +94,50 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let padding:CGFloat = 10
+        let collectionviewSize = self.collectionView.bounds.width
+        let columnNum = 3
+        let itemWidth: CGFloat = (collectionviewSize - padding * CGFloat(columnNum - 1)) / CGFloat(columnNum)
         
-        return CGSizeMake(110, 110)
+        return CGSizeMake(itemWidth, itemWidth)
     }
     
     
+    //控制cell Y-padding之間的距離
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat{
+        
+        return 10
+    
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat{
+        return 10
+    }
     
     
-    
-    
+    func initAudio() {
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path!)!)
+            
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+        } catch let err as NSError{
+            print(err.description)
+        }
+        
+    }
 
+    @IBAction func musicBtnPressed(sender: UIButton) {
+        if musicPlayer.playing{
+            musicPlayer.stop()
+            sender.alpha = 0.2
+        }else{
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+    }
 }
 
