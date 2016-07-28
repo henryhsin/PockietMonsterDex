e4dcfd04b8603bfb571ef2e57e10500577ad9948
Hsin <hooy123456@gmail.com>
2016-07-28 08:14:06 +0800

    part11 add new properties to Pokemon object and connect mainStoryBoard to DetailVC

--- PokietMonDect/PokemonDetailVC.swift
+++ PokietMonDect/PokemonDetailVC.swift
@@ -10,7 +10,45 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
+    @IBOutlet weak var pokeNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var pokeImg: UIImageView!
+    
+    @IBOutlet weak var pokeDescription: UILabel!
+    
+    @IBOutlet weak var pokemonType: UILabel!
+    
+    @IBOutlet weak var pokemonDef: UILabel!
+    
+    
+    @IBOutlet weak var pokemonHeight: UILabel!
+    
+    
+    @IBOutlet weak var pokemonPokedexID: UILabel!
+    
+    
+    @IBOutlet weak var pokemonWeight: UILabel!
+    
+    
+    @IBOutlet weak var pokemonAttack: UILabel!
+    
+    
+    
+    @IBOutlet weak var pokemonCurrEvoImg: UIImageView!
+    
+    @IBOutlet weak var pokemonNextEvoImg: UIImageView!
+    
+    @IBOutlet weak var evoLabel: UILabel!
+    
+    @IBAction func backBtn(sender: UIButton) {
+        //pop our detail view and load main view again
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     var detailPokemon: Pokemon!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
